2190cb3aa2bf1033ff663b29ebe569878d1c0896
51AN <sianashsad@iut-dhaka.edu>
2025-03-05 00:36:35 +0600

    Implement Adapter and Decorator Patterns

---
+++ AdapterDesignPattern/AdapterDesignPattern/Program.cs
@@ -0,0 +1,48 @@
+﻿
+public class TemperatureCelsius
+{
+    public TemperatureCelsius()
+    {
+
+    }
+
+    public double getTemp()
+    {
+        return 25.0;
+    }
+}
+
+public interface iTemperatureService
+{
+    double getTemperature();
+}
+
+public class TemperatureAdapter : iTemperatureService
+{
+    private readonly TemperatureCelsius temperatureCelsius;
+
+    public TemperatureAdapter (TemperatureCelsius temperatureCelsius)
+    {
+        this.temperatureCelsius = temperatureCelsius;
+    }
+
+    public double getTemperature()
+    {
+        //Implementation of adaption
+        double celsius = temperatureCelsius.getTemp();
+        return (celsius * 9 / 5) + 32;
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        TemperatureCelsius temperatureCelsius = new TemperatureCelsius();
+
+        TemperatureAdapter temperatureAdapter = new TemperatureAdapter(temperatureCelsius);
+        Console.WriteLine("Temperature in Celsius: " + temperatureCelsius.getTemp());
+        Console.WriteLine("Temperature in Farenheit: " + temperatureAdapter.getTemperature());
+    }
+}
---
+++ DecoratorPattern/DecoratorPattern/Program.cs
@@ -0,0 +1,110 @@
+﻿//First we make the base coffee interface to determine the behavior
+
+using System.Transactions;
+
+public interface Icoffee
+{
+    string getDescription();
+    double getPrice();
+
+}
+
+//Now create the basic coffee class
+
+public class BasicCoffee : Icoffee
+{
+    public string getDescription()
+    {
+        return "Simple Coffee";
+    }
+
+    public double getPrice()
+    {
+        return 20.0;
+    }
+}
+
+// Now create the decorator abstract class as the decoration middlepoint
+
+public abstract class CoffeeDecorator : Icoffee
+{
+    private readonly Icoffee _coffee;
+
+    public CoffeeDecorator(Icoffee coffee)
+    {
+        _coffee = coffee;
+    }
+
+    public virtual string getDescription()
+    {
+        return _coffee.getDescription();
+    }
+
+    public virtual double getPrice()
+    {
+        return _coffee.getPrice();
+    }
+
+}
+
+// Now to make specific decorators
+
+public class MilkDecorator : CoffeeDecorator
+{
+
+    public MilkDecorator(Icoffee coffee) : base(coffee)
+    {
+
+    }
+
+    public override string getDescription()
+    {
+        return base.getDescription() + " + Milk";
+    }
+
+    public override double getPrice()
+    {
+        return base.getPrice() + 10;
+    }
+}
+
+public class SugarDecorator : CoffeeDecorator
+{
+
+    public SugarDecorator(Icoffee coffee) : base(coffee)
+    {
+
+    }
+
+    public override string getDescription()
+    {
+        return base.getDescription() + " + Sugar";
+    }
+
+    public override double getPrice()
+    {
+        return base.getPrice() + 5;
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        Icoffee coffee = new BasicCoffee();
+
+        Console.WriteLine("Coffee Type: " + coffee.getDescription() + " and  price: " + coffee.getPrice());
+
+        coffee = new SugarDecorator(coffee);
+
+        Console.WriteLine("Coffee Type: " + coffee.getDescription() + " and  price: " + coffee.getPrice());
+
+        coffee = new MilkDecorator(coffee);
+
+        Console.WriteLine("Coffee Type: " + coffee.getDescription() + " and  price: " + coffee.getPrice());
+
+
+
+    }
+}
