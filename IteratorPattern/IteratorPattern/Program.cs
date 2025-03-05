3abe1ab1d225c40c449931e0a25688473d0004f7
51AN <sianashsad@iut-dhaka.edu>
2025-03-05 22:44:58 +0600

    Implemented Iterator design pattern

---
+++ IteratorPattern/IteratorPattern/Program.cs
@@ -0,0 +1,87 @@
+﻿public class Book
+{
+    public string Title { get; }
+    public Book(string _title)
+    {
+        Title = _title;
+    }
+}
+
+//Define iterator interface
+
+public interface IBookIterator
+{
+    bool MoveNext();
+    Book Next();
+}
+
+// Define Collection Interface
+
+public interface IBookCollection
+{
+    IBookIterator CreateIterator();
+}
+
+// Creat Concrete Iterator
+
+public class BookIterator : IBookIterator
+{
+    private readonly List<Book> books;
+    private int currentIndex = 0;
+
+    public BookIterator(List<Book> books)
+    {
+        this.books = books ?? new List<Book>(); //handling the case of null 
+    }
+
+    public bool MoveNext()
+    {
+        return books != null && currentIndex < books.Count;
+    }
+
+    public Book Next()
+    {
+        if (!MoveNext())
+        {
+            throw new InvalidOperationException("No more books to iterate over.");
+        }
+        return books[currentIndex++];
+    }
+}
+
+// Create concrete book iterator class
+public class BookCollection : IBookCollection
+{
+    private readonly List<Book> books = new List<Book>();
+
+    public void AddBook (Book book)
+    {
+        books.Add(book);
+    }
+
+    public IBookIterator CreateIterator()
+    {
+        return new BookIterator(books);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        BookCollection library = new BookCollection();
+        library.AddBook(new Book("Design Patterns in C#"));
+        library.AddBook(new Book("Clean Code"));
+        library.AddBook(new Book("The Pragmatic Programmer"));
+        library.AddBook(new Book("Twilight Saga"));
+
+        IBookIterator bookIterator = library.CreateIterator();
+
+        while (bookIterator.MoveNext())
+        {
+            Book book = bookIterator.Next();
+            Console.WriteLine($"Book: {book.Title}");
+        }
+
+    }
+}
