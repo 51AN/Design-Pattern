245b7e41fab2e59f17bb2e061c3585a4df4d282c
51AN <sianashsad@iut-dhaka.edu>
2025-05-10 16:06:35 +0600

    creating required entities for room allocation

--- 200042151_FinalTask/200042151_FinalTask/Program.cs
+++ 200042151_FinalTask/200042151_FinalTask/Program.cs
@@ -4,12 +4,38 @@ using System.Collections.Generic;
 
 public class Room
 {
+    public string Id { get; }
+    public string Location { get; }
+    public int Capacity { get; }
+    public int OccupiedSeats { get; private set; }
+
+    public bool IsActive { get; set; }
+
+    public Room(string id, string location, int capacity)
+    {
+        Id = id;
+        Location = location;
+        Capacity = capacity;
+        OccupiedSeats = 0;
+        IsActive = false;
+    }
+
+
 
 }
 
 public class Center
 {
+    public string Name { get; }
+    public List<Room> Rooms { get; }
 
+    public Center(string name)
+    {
+        Name = name;
+        Rooms = new List<Room>();
+    }
+
+    public void AddRoom(Room room) => Rooms.Add(room);
 }
 
 
